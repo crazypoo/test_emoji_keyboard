071016cde9008230566a54f98a7a10ff3871468b
crazypoo <273277355@qq.com>
2013-07-04 14:48:37 +0800

    Initial Commit

---
+++ asd/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  asd
+//
+//  Created by crazypoo on 13-7-4.
+//  Copyright (c) 2013年 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ asd/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  asd
+//
+//  Created by crazypoo on 13-7-4.
+//  Copyright (c) 2013年 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
