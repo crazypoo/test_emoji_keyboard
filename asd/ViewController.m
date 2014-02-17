e3d81b0d304916d782695783c0532b89388934aa
crazypoo <273277355@qq.com>
2014-02-17 17:48:41 +0800

    test_emoji_keyboard
    測試當前為什麼鍵盤類型！！！！！

--- asd/ViewController.m
+++ asd/ViewController.m
@@ -17,9 +17,46 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWasChange:) name:UIKeyboardDidChangeFrameNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeInputMode:) name:UITextInputCurrentInputModeDidChangeNotification object:nil];
+    UITextView *lol = [[UITextView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    [self.view addSubview:lol];
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
+UIView *resignFirstResponder(UIView *theView)
+{
+    if([theView isFirstResponder])
+    {
+        [theView resignFirstResponder];
+        return theView;
+    }
+    for(UIView *subview in theView.subviews)
+    {
+        UIView *result = resignFirstResponder(subview);
+        if(result) return result;
+    }
+    return nil;
+}
+
+-(void)changeInputMode:(NSNotification *)notification{
+    NSString *inputMethod = [[UITextInputMode currentInputMode] primaryLanguage];
+        NSString *modeIdentifier = [[UITextInputMode currentInputMode] respondsToSelector:@selector(identifier)] ? (NSString *)[[UITextInputMode currentInputMode] performSelector:@selector(identifier)] : nil;
+    
+    NSLog(@"inputMethod=%@----%@",inputMethod,modeIdentifier);
+    if([inputMethod isEqualToString:@"emoji"])
+    {
+        [UIView setAnimationsEnabled:NO];
+        UIView *resigned = resignFirstResponder([UIApplication sharedApplication].keyWindow);
+        [resigned becomeFirstResponder];
+        [UIView setAnimationsEnabled:YES];
+    }
+    if ([inputMethod isEqualToString:@"emoji"])
+    {
+        NSLog(@"---------------");
+    }
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
